48afca340aa9dd6d8c4303ca1cb23d68de06143c
Khushi Patel <138273454+khupatel@users.noreply.github.com>
2024-11-07 20:41:31 -0500

    Added Flask integration and ensured program works

---
+++ app.py
@@ -0,0 +1,77 @@
+# Document Parsing
+# pip install PyPDF2 on local terminal
+from PyPDF2 import PdfReader
+from flask import Flask, request, render_template
+
+# Open the PDF file
+file_path = "C:/Users/7849l/Desktop/test.pdf"
+extracted_texts = {}
+
+# Extract text from each page
+# iterating through each page and extracting the text
+# printing the text on each page
+# extracted text is stored by page
+
+def extract_text_from_pdf(file_path):
+    pdf_reader = PdfReader(file_path)
+    for page_num, page in enumerate(pdf_reader.pages):
+        page_text = page.extract_text()
+        # print(f"Page {page_num + 1}:\n{page_text}\n")
+        extracted_texts[page_num + 1] = page_text
+    return extracted_texts
+
+# Load and preprocess the documents
+extracted_texts = extract_text_from_pdf(file_path)
+
+# Handling the Query
+# Retrieve the user's question
+# stripping white spaces and converting to lowercase
+def handle_query(query):
+    results = search_documents(query, extracted_texts)
+
+    print(f"Search results: {results}")
+    return results
+
+# searching the document
+# catches the first time the word is used in the document
+def search_documents(query, texts, snippet_length=30):
+    results = []
+
+    for page_num, text in texts.items():
+        text = text.lower()
+        if query in text:
+            # find the index where the query was found
+            start_index = text.index(query)
+
+            # creating a snippet
+            # calculate the start and end indices for the snippet
+            start_snippet = max(start_index - snippet_length, 0)
+            end_snippet = start_index + len(query) + snippet_length
+
+            # extract the snippet from the original text
+            snippet = text[start_snippet:end_snippet].strip()
+
+            results.append((page_num, snippet))
+    return results
+
+# Flask app integration
+app = Flask(__name__)
+
+# GET request: Default request type when a page is loaded/refreshed; displays the search form without any results
+# POST request: Triggered when user submits a query via the form; tells Flask to process the form data and display the results
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    query = None
+    results = []
+
+    # if request is a POST (form was submitted)
+    if request.method == "POST":
+        query = request.form["query"].strip().lower()
+        results = handle_query(query)
+
+    return render_template("index.html", query=query, results=results)
+
+# running the flask app locally
+if __name__ == "__main__":
+    app.run(port=81, debug=True)
